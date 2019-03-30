571677a2be5ab8adde5389586ec1df004e00f106
Kirsty Adair <kirstyaadair@gmail.com>
2019-03-30 00:57:52 +0000

    oops

--- BlockyPrototype/Assets/Scripts/RequirementsGeneratorScript.cs
+++ BlockyPrototype/Assets/Scripts/RequirementsGeneratorScript.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RequirementsGenerationScript : MonoBehaviour
+public class RequirementsGeneratorScript : MonoBehaviour
 {
+    public GameObject chooseRequirementsPanel;
+    public GameObject requirementsPanel;
     public Text requiredFloorsText;
     public Text randomFeatureText;
+    public Text floorsText;
+    public Text featureText;
     public string[] features;
     public int requiredFloors;
     public string feature;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        chooseRequirementsPanel.SetActive(true);
+        requirementsPanel.SetActive(false);
+        RandomlyGenerateRequirements();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void RandomlyGenerateRequirements()
@@ -33,6 +39,13 @@ public class RequirementsGenerationScript : MonoBehaviour
         int randomArrayPos = Mathf.RoundToInt(Random.Range(0, features.Length));
         feature = features[randomArrayPos];
         randomFeatureText.text = "It must also have a " + feature + ".";
+    }
 
+    public void SaveRequirements()
+    {
+        chooseRequirementsPanel.SetActive(false);
+        requirementsPanel.SetActive(true);
+        floorsText.text = "Required floors: " + requiredFloors;
+        featureText.text = "Required feature: " + feature;
     }
 }
