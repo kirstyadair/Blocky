0bbc8fe1770c9365a594ec648571c0fa519d3314
Kirsty Adair <kirstyaadair@gmail.com>
2019-05-12 00:52:39 +0100

    burning fix

--- BlockyPrototype/Assets/Scripts/Cube Type Script/BurningCubeScript.cs
+++ BlockyPrototype/Assets/Scripts/Cube Type Script/BurningCubeScript.cs
@@ -17,5 +17,9 @@ public class BurningCubeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y != -0.8799995f)
+        {
+            transform.position = new Vector3(transform.position.x, -0.8799995f, transform.position.z);
+        }
     }
 }
